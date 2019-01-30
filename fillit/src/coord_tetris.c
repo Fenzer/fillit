0333af747dd842f724ad12a492013ffcd3281e60
Lisa FERRERI <liferrer@e2r12p23.42.fr>
2019-01-30 18:15:02 +0100

    Debut de coord_tetris, il manque le move en haut a gauche

--- fillit/src/coord_tetris.c
+++ fillit/src/coord_tetris.c
@@ -6,22 +6,34 @@
 /*   By: liferrer <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/30 16:55:23 by liferrer          #+#    #+#             */
-/*   Updated: 2019/01/30 17:01:35 by liferrer         ###   ########.fr       */
+/*   Updated: 2019/01/30 18:13:46 by liferrer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	coord_tetris(char	**str)
+#include "fillit.h"
+
+int	coord_tetris(char **str, t_tetri **tetris)
 {
+	int i;
 	int x;
 	int y;
 
 	i = 0;
+	x = 0;
+	y = 0;
 	while ((*str)[i])
 	{
 		if ((*str)[x] == '#')
-			//on stock x et le y dans le tableau
-		if ((*str)[i] == '\n')
+		{
+			(*tetris)->coord[i][0] == y;
+			(*tetris)->coord[i][1] == x;
+			i++;
+		}
+		if ((*str)[x] == '\n')
+		{
 			y++;
-
+			x = 0;
+		}
+		x++;
 	}
 }
