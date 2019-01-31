f3318ca016c5a0db5e05763cca34001cbcb6b668
Lisa FERRERI <liferrer@e1r8p20.42.fr>
2019-01-31 16:55:34 +0100

    fct recuperation des coord + mv en haut a gauche tested

--- fillit/src/coord_tetris.c
+++ fillit/src/coord_tetris.c
@@ -6,34 +6,64 @@
 /*   By: liferrer <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/30 16:55:23 by liferrer          #+#    #+#             */
-/*   Updated: 2019/01/30 18:13:46 by liferrer         ###   ########.fr       */
+/*   Updated: 2019/01/31 16:39:57 by liferrer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "fillit.h"
+#include "../fillit.h"
+#include <stdio.h>
 
-int	coord_tetris(char **str, t_tetri **tetris)
+void	get_new_coord(int min_y, int min_x, t_tetri **tetris)
 {
 	int i;
-	int x;
-	int y;
 
 	i = 0;
-	x = 0;
-	y = 0;
+	while (i < 4)
+	{
+		(*tetris)->coord[i][0] -= min_y;
+		(*tetris)->coord[i][1] -= min_x;
+		i++;
+	}
+}
+
+void	coord_tetris(char **str, t_tetri **tetris)
+{
+	int i;
+	int j;
+	int	min_x;
+	int min_y;
+
+	i = 0;
+	j = 0;
+	min_x = 5;
+	min_y = 5;
 	while ((*str)[i])
 	{
-		if ((*str)[x] == '#')
+		if ((*str)[i] == '#')
 		{
-			(*tetris)->coord[i][0] == y;
-			(*tetris)->coord[i][1] == x;
-			i++;
+			(*tetris)->coord[j][0] = i / 5;
+			(*tetris)->coord[j][1] = i < 5 ? i : i % 5;
+			if ((*tetris)->coord[j][0] < min_y)
+				min_y = (*tetris)->coord[j][0];
+			if ((*tetris)->coord[j][1] < min_x)
+				min_x = (*tetris)->coord[j][1];
+			j++;
 		}
-		if ((*str)[x] == '\n')
-		{
-			y++;
-			x = 0;
-		}
-		x++;
+		i++;
 	}
+	get_new_coord(min_y, min_x, tetris);
+}
+
+int	main(int ac, char **argv)
+{
+	char	*str;
+	int 	i;
+	t_tetri *tetris = malloc(sizeof(t_tetri));
+
+	i = -1;
+	str = ft_strdup(".##.\n..#.\n..#.\n....\n");
+	coord_tetris(&str, &tetris);
+	while (++i < 4)
+		printf("y = %d ; x = %d\n", tetris->coord[i][0], tetris->coord[i][1]);
+	return (0);
 }
