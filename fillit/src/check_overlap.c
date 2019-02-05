bd3058707f61a0b8d9e270452c516ceffb1e391b
Vito FERRERI # Aprile <VitoFERRERI@MacBook-Pro-de-ZacharieFirat.local>
2019-02-05 16:02:17 +0100

    fonction check overlap testee + main de test

--- fillit/src/check_overlap.c
+++ fillit/src/check_overlap.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "fillit.h"
+#include "../fillit.h"
 
 int	check_overlap(t_tetri *forme1, t_tetri *forme2)
 {
@@ -19,11 +19,38 @@ int	check_overlap(t_tetri *forme1, t_tetri *forme2)
 	i = 0;
 	while (i < 4)
 	{
-		if ((*forme1)[i].coord[i][0] == (*forme2)[i].coord[i][0]
-				&& (*forme1)[i].coord[i][1] == (*forme2)[i].coord[i][1])
+		if ((*forme1).coord[i][0] == (*forme2).coord[i][0]
+			 && (*forme1).coord[i][1] == (*forme2).coord[i][1]) 
 			return (0);
 		else
 			i++;
 	}
 	return (1);
+}
+
+int	main(void)
+{
+	t_tetri	forme1;
+	t_tetri forme2;
+
+	forme1.coord[0][0] = 0;
+	forme1.coord[0][1] = 1;
+	forme1.coord[1][0] = 1;
+	forme1.coord[1][1] = 0;
+	forme1.coord[2][0] = 1;
+	forme1.coord[2][1] = 1;
+	forme1.coord[3][0] = 1;
+	forme1.coord[3][1] = 2;
+
+	forme2.coord[0][0] = 0;
+	forme2.coord[0][1] = 3;
+	forme2.coord[1][0] = 1;
+	forme2.coord[1][1] = 3;
+	forme2.coord[2][0] = 2;
+	forme2.coord[2][1] = 3;
+	forme2.coord[3][0] = 3;
+	forme2.coord[3][1] = 3;
+
+	check_overlap(&forme1, &forme2);
+	return (0);
 }
