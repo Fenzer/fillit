f29306abea75005bf490e05a9a01c5dec43a7a58
Fen <fepinson@student.42.us.org>
2019-04-24 14:30:46 +0200

    algo

--- src/main.c
+++ src/main.c
@@ -6,11 +6,36 @@
 /*   By: fepinson <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/19 14:54:51 by fepinson          #+#    #+#             */
-/*   Updated: 2019/04/24 03:02:19 by fepinson         ###   ########.fr       */
+/*   Updated: 2019/04/24 14:30:23 by fepinson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../fillit.h"
+
+int		ft_sqrt(int n)
+{
+	int	large;
+	int	small;
+
+	if (n < 0)
+		return (0);
+	if (n < 2)
+		return (n);
+	else
+	{
+		small = ft_sqrt(n >> 2) << 1;
+		large = small + 1;
+		if (small * large > n)
+			return (small);
+		else
+			return (large);
+	}
+}
+
+int		solve_square(t_tetri *tetri, int i, int size)
+{
+
+}
 
 int	free_return(void *p, int i)
 {
@@ -20,8 +45,7 @@ int	free_return(void *p, int i)
 
 int	solve(t_tetri *tetri, int i, int sz)
 {
-	if (find_overlap(tetri, i) && check_all_fit(tetri, i, sz))
-		
+	return (0);
 }
 
 int	main(int argc, const char *argv[])
@@ -40,6 +64,6 @@ int	main(int argc, const char *argv[])
 			return (free_return((void *)tetri, 42));
 	if (i == 25 && read_load_tetri(fd, tetri + i, i))
 		return (free_return((void *)tetri, 42));
-	solve(tetri, i, 2);
+	solve(tetri, i, ft_sqrt(i * 4));
 	return (free_return((void *)tetri, 0));
 }
